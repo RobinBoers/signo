5fae5c0f4c7f687badaa594c793a93cabe0c19ce
Robin Boers <robindev2019@outlook.com>
2024-06-10 22:09:55 +0200

    Setup documentation

--- mix.exs
+++ mix.exs
@@ -2,11 +2,17 @@ defmodule Signo.MixProject do
   use Mix.Project
 
   def project, do: [
+    name: "Signo",
     app: :signo,
     version: "0.1.0",
     elixir: "~> 1.16",
     start_permanent: Mix.env() == :prod,
-    deps: deps()
+    deps: deps(),
+
+    # Docs
+    source_url: "https://git.dupunkto.org/axcelott/signo",
+    homepage_url: "https://roblog.nl/projects",
+    docs: docs()
   ]
 
   def application, do: [
@@ -14,6 +20,14 @@ defmodule Signo.MixProject do
   ]
 
   defp deps, do: [
-    {:typed_struct, "~> 0.3.0"}
+    {:typed_struct, "~> 0.3.0"},
+    {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+  ]
+
+  defp docs, do: [
+    main: "Signo",
+    api_reference: false,
+    authors: ["Robijntje"],
+    formatters: ["html"]
   ]
 end
