29e4289d8446d8aea87a6de2f264985d40c1af44
Robin Boers <robindev2019@outlook.com>
2024-06-30 11:40:11 +0200

    Save path info in `Signo.Position`
    I just noticed I switched around two commit messages while commiting. Whoops!

--- lib/signo/position.ex
+++ lib/signo/position.ex
@@ -4,22 +4,33 @@ defmodule Signo.Position do
   """
   use TypedStruct
 
+  @type location :: Path.t() | :runtime
+
   typedstruct enforce: true do
-    field :path, String.t(), default: "internal"
-    field :row, non_neg_integer(), default: 0
-    field :col, non_neg_integer(), default: 0
+    field :path, location(), default: :runtime
+    field :row, non_neg_integer(), default: 1
+    field :col, non_neg_integer(), default: 1
+  end
+
+  @doc """
+  Constructs a new `Signo.Position`.
+  """
+  @spec new(location()) :: t()
+  def new(path) when is_binary(path) or path == :runtime do
+    %__MODULE__{path: path}
   end
 
   @doc """
   Updates the recorded position given either a single grapheme,
   or a collected list of graphemes.
   """
-  @spec increment(t(), String.grapheme() | [String.grapheme()]) :: t()
+  @spec increment(t(), [String.grapheme()]) :: t()
   def increment(pos, lexeme) when is_list(lexeme) do
     Enum.reduce(lexeme, pos, &increment(&2, &1))
   end
 
-  def increment(pos, "\n"), do: %__MODULE__{pos | row: pos.row + 1, col: 0}
+  @spec increment(t(), String.grapheme()) :: t()
+  def increment(pos, "\n"), do: %__MODULE__{pos | row: pos.row + 1, col: 1}
   def increment(pos, _chr), do: %__MODULE__{pos | col: pos.col + 1}
 end
 
