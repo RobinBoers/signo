c2855f7dc642338d2484883be75d4e37b7ff0b41
Robin Boers <robindev2019@outlook.com>
2024-07-01 17:56:23 +0200

    Add constructors to AST

--- lib/signo/ast.ex
+++ lib/signo/ast.ex
@@ -14,8 +14,14 @@ defmodule Signo.AST do
     @moduledoc """
     A list of expressions.
     """
+
     typedstruct enforce: true do
       field :expressions, [AST.expression()]
+    end
+
+    @spec new([AST.expression()]) :: t()
+    def new(expressions) do
+      %__MODULE__{expressions: expressions}
     end
   end
 
@@ -23,8 +29,16 @@ defmodule Signo.AST do
     @moduledoc """
     A literal value, as an elixir `t:term/0`.
     """
+
+    @type value :: binary() | integer() | float() | boolean()
+
     typedstruct enforce: true do
-      field :value, binary() | integer() | float() | boolean()
+      field :value, value()
+    end
+
+    @spec new(value()) :: t()
+    def new(value) do
+      %__MODULE__{value: value}
     end
   end
 
@@ -32,8 +46,14 @@ defmodule Signo.AST do
     @moduledoc """
     A reference to variable or function in scope.
     """
+
     typedstruct enforce: true do
       field :reference, String.t()
+    end
+
+    @spec new(String.t()) :: t()
+    def new(ref) do
+      %__MODULE__{reference: ref}
     end
   end
 
@@ -41,10 +61,20 @@ defmodule Signo.AST do
     @moduledoc """
     An execution branch based a on condition.
     """
+
     typedstruct enforce: true do
       field :condition, AST.expression()
       field :then, AST.expression()
       field :else, AST.expression()
+    end
+
+    @spec new(AST.expression(), AST.expression(), AST.expression()) :: t()
+    def new(condition, then, otherwise) do
+      %__MODULE__{
+        condition: condition,
+        then: then,
+        else: otherwise
+      }
     end
   end
 
@@ -52,20 +82,37 @@ defmodule Signo.AST do
     @moduledoc """
     A declaration of a variable in scope.
     """
+
     typedstruct enforce: true do
       field :reference, String.t()
       field :value, AST.expression()
     end
+
+    @spec new(String.t(), AST.expression()) :: t()
+    def new(ref, value) do
+      %__MODULE__{
+        reference: ref,
+        value: value
+      }
+    end
   end
 
-  defmodule Def do
+  defmodule Lambda do
     @moduledoc """
     A declaration of a function in scope.
     """
+
     typedstruct enforce: true do
-      field :reference, String.t()
       field :arguments, [Symbol.t()]
       field :body, AST.expression()
+    end
+
+    @spec new([Symbol.t()], AST.expression()) :: t()
+    def new(args, body) do
+      %__MODULE__{
+        arguments: args,
+        body: body
+      }
     end
   end
 end
