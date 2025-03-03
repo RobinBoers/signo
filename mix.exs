fa554c83c2b7f140b570195da7a6fcae872daaf5
Robin <robindev2019@outlook.com>
2025-03-03 17:12:34 +0100

    Bump version number

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Signo.MixProject do
   def project, do: [
     name: "Signo",
     app: :signo,
-    version: "0.0.1",
+    version: "0.0.2",
     elixir: "~> 1.16",
     start_permanent: Mix.env() == :prod,
     dialyzer: [plt_add_apps: [:mix]],
